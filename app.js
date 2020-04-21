0858c611023b75743cd2da388ae18989a2e9588b
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-04-21 17:15:00 +0900

    PaintJS 2.1

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const canvas = document.getElementById("jsCanvas");
 
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.linewidth = 2.5;
+
 let painting = false;
 
 function stopPainting(event) {
   painting = false;
 }
 
+function startPainting(event) {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.strokeStyle();
+  }
 }
 
 function onMouseDown(event) {
   stopPainting();
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
